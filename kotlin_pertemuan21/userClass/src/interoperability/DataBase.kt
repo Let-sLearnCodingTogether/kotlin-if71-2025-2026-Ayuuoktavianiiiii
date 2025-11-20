8ea4730317f87244b3ac99305badb51c3559d3e3
Ayuuoktavianiiiii <ayuuoktavianiiiii@mhs.mdp.ac.id>
2025-11-20 20:16:32 +0700

    pertemuan 21

---
+++ kotlin_pertemuan21/userClass/src/interoperability/DataBase.kt
@@ -0,0 +1,10 @@
+package interoperability
+
+class DataBase {
+    companion object {
+        @JvmStatic
+        fun getConnectionUrl(): String {
+            return "db connection url"
+        }
+    }
+}
